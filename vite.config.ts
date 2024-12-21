18fd4cfb4c0eb90ede68d87bcd930f7361a7de3c
teru <85771338+teruteru214@users.noreply.github.com>
2024-12-21 16:07:06 +0900

    Refactor: ビルド設定を見直す

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react()],
+	build: {
+		outDir: "dist",
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
